f7ca5bdf8dae8342820eda9e5adfae1f9a38adc6
Aadinath1234 <aadeshsatpute375@gmail.com>
2025-07-24 22:39:16 +0530

    adding files with aadesh

--- src/state/store.tsx
+++ src/state/store.tsx
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from './slices/authSlice';
 import roadmapReducer from "./slices/roadmapSlice"; 
 import  analyticsReducer  from './slices/analyticsSlice';
+import bookmarkReducer from './slices/bookmarkSlice'; 
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     roadmap:roadmapReducer,
     analytics: analyticsReducer, 
+    bookmark: bookmarkReducer
   },
 });
 

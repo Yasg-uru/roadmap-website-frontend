40232f30b48387c567049d66789980ce76d0f42a
Yash pawar <yashpawar12122004@gmail.com>
2025-12-23 00:15:16 +0530

    fixed errors for deployment

--- src/helper/axiosInstance.ts
+++ src/helper/axiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 const 
 axiosInstance = axios.create({
-//   baseURL: "https://advance-online-learning-platform.vercel.app",
-  baseURL: "http://localhost:8000",
+  baseURL: "https://roadmap-backend-1-9rcd.onrender.com",
+  // baseURL: "http://localhost:8000",
   withCredentials: true,
 });
 export default axiosInstance;
